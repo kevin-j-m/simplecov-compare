8aea29fe350e8ac51ec6267cd7302e3f60503f79
Kevin Murphy <murphy.kevin.mail@gmail.com>
2026-02-05 15:50:01 -0500

    bundle gem simplecov-compare

---
+++ lib/simplecov/compare.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "compare/version"
+
+module Simplecov
+  module Compare
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/simplecov/compare/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Simplecov
+  module Compare
+    VERSION = "0.1.0"
+  end
+end
