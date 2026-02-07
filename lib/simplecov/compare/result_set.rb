c24a1511a9b9d058eae31e1ddfc6ebe29eb8e997
Kevin Murphy <murphy.kevin.mail@gmail.com>
2026-02-07 15:05:08 -0500

    a result set can calculate its overall coverage

--- lib/simplecov/compare/result_set.rb
+++ lib/simplecov/compare/result_set.rb
@@ -7,8 +7,12 @@ module Simplecov
     class ResultSet
       # https://github.com/simplecov-ruby/simplecov/blob/main/lib/simplecov/file_list.rb
 
-      def initialize(file_path)
+      def initialize(file_path: nil, coverage_data: nil)
         @file_path = file_path
+
+        if coverage_data
+          @coverage_data = coverage_data
+        end
       end
 
       def files
@@ -19,6 +23,20 @@ module Simplecov
           .map { |filename, coverage| FileResult.new(filename, coverage_data: JSON.parse(JSON.dump(coverage))) }
           .compact
           .sort_by { _1.filename }
+      end
+
+      def num_relevant_lines
+        files.sum { _1.relevant_lines.size }
+      end
+
+      def num_covered_lines
+        files.sum { _1.covered_lines.size }
+      end
+
+      def lines_covered_percent
+        return 100 if num_relevant_lines.zero?
+
+        ((num_covered_lines / num_relevant_lines.to_f) * 100).round(2)
       end
 
       private
--- test/simplecov/compare/result_set_test.rb
+++ test/simplecov/compare/result_set_test.rb
@@ -7,7 +7,7 @@ module Simplecov
     describe ResultSet do
       describe "#files" do
         it "parses the JSON file and provides information on all the files in the file" do
-          set = ResultSet.new("test/fixtures/sample_resultset.json")
+          set = ResultSet.new(file_path: "test/fixtures/sample_resultset.json")
 
           coverage_result = [
             ["/path/to/app/helpers/application_helper.rb", 100.0],
@@ -15,6 +15,147 @@ module Simplecov
             ["/path/to/app/models/other_model.rb", 50.0],
           ]
           assert_equal coverage_result, set.files.map { [_1.filename, _1.lines_covered_percent] }
+        end
+
+        it "coverts the provided coverage data hash into file results" do
+          set = ResultSet.new(
+            coverage_data: {
+              "/path/to/app/helpers/application_helper.rb" => {
+                "lines" => [
+                  1,
+                  nil,
+                ]
+              },
+              "/path/to/app/models/model_name.rb" => {
+                "lines" => [
+                  1,
+                  1,
+                  nil,
+                  1,
+                  1,
+                  nil,
+                  1,
+                  6,
+                  nil,
+                  nil,
+                  nil,
+                  1,
+                  nil,
+                  7,
+                  12,
+                  0,
+                  nil,
+                  0,
+                  nil,
+                  nil,
+                  nil,
+                  nil,
+                ],
+              },
+              "/path/to/app/models/other_model.rb" => {
+                "lines" => [
+                  7,
+                  5,
+                  5,
+                  4,
+                  4,
+                  4,
+                  nil,
+                  0,
+                  0,
+                  0,
+                  nil,
+                  0,
+                  0,
+                  0,
+                  nil
+                ],
+                "branches": {}
+              }
+            },
+          )
+
+          coverage_result = [
+            ["/path/to/app/helpers/application_helper.rb", 100.0],
+            ["/path/to/app/models/model_name.rb", 81.82],
+            ["/path/to/app/models/other_model.rb", 50.0],
+          ]
+          assert_equal coverage_result, set.files.map { [_1.filename, _1.lines_covered_percent] }
+        end
+      end
+
+      describe "#num_relevant_lines" do
+        it "counts the relevant lines across files" do
+          set = ResultSet.new(
+            coverage_data: {
+              "file1.rb" => {
+                "lines" => [
+                  1,
+                  nil,
+                ]
+              },
+              "file2.rb" => {
+                "lines" => [
+                  1,
+                  0,
+                  0,
+                  nil,
+                ],
+              },
+            },
+          )
+
+          assert_equal 4, set.num_relevant_lines
+        end
+      end
+
+      describe "#num_covered_lines" do
+        it "counts the covered lines across files" do
+          set = ResultSet.new(
+            coverage_data: {
+              "file1.rb" => {
+                "lines" => [
+                  1,
+                  nil,
+                ]
+              },
+              "file2.rb" => {
+                "lines" => [
+                  1,
+                  0,
+                  0,
+                  nil,
+                ],
+              },
+            },
+          )
+
+          assert_equal 2, set.num_covered_lines
+        end
+      end
+
+      describe "#lines_covered_percent" do
+        it "calculates the lines coverage for the full set" do
+          set = ResultSet.new(
+            coverage_data: {
+              "file1.rb" => {
+                "lines" => [
+                  1,
+                  nil,
+                ]
+              },
+              "file2.rb" => {
+                "lines" => [
+                  1,
+                  0,
+                  0,
+                  nil,
+                ],
+              },
+            },
+          )
+
+          assert_equal 50.0, set.lines_covered_percent
         end
       end
     end
